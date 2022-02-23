829e53bc6d72e139669b0fd870fd7aca8936a38a
Anton Dukeman <anton.dukeman@gmail.com>
2022-02-23 00:01:10 -0800

    Filter on ppp channels only

--- app/src/main/java/ppp/stats/PPPBot.java
+++ app/src/main/java/ppp/stats/PPPBot.java
@@ -6,6 +6,7 @@ import discord4j.core.DiscordClient;
 import discord4j.core.GatewayDiscordClient;
 import discord4j.core.event.domain.message.MessageCreateEvent;
 import discord4j.core.object.entity.Message;
+import discord4j.core.object.entity.channel.TextChannel;
 import ppp.stats.data.IDataManager;
 import ppp.stats.data.InMemoryDataManager;
 import ppp.stats.logging.ILogger;
@@ -35,6 +36,10 @@ public class PPPBot {
     public void startListening() {
         this.gateway.getEventDispatcher().on(MessageCreateEvent.class)
             .map(MessageCreateEvent::getMessage)
+            .filter(message -> message.getChannel().block() instanceof TextChannel)
+            .filter(message -> {
+                String chanName = ((TextChannel)message.getChannel().block()).getName();
+                return chanName.equals("periodic-perplexing-parlances") || chanName.equals("ppp-stats-test"); })
             .filter(message -> message.getAuthor().map(user -> !user.isBot()).orElse(false))
             .filter(message -> message.getId().asLong() > this.currentMaxId)
             .subscribe(message -> {
