05999817da64a2f53623582849567a58821d094e
Anton Dukeman <anton.dukeman@gmail.com>
2022-03-06 00:26:49 -0800

    Commit prod run updates

--- app/src/main/java/ppp/stats/logging/SystemOutLogger.java
+++ app/src/main/java/ppp/stats/logging/SystemOutLogger.java
@@ -1,30 +1,32 @@
 package ppp.stats.logging;
 
+import java.time.LocalTime;
+
 public class SystemOutLogger implements ILogger {
     public static SystemOutLogger shared = new SystemOutLogger();
     
     @Override
     public void error(String log) {
-        System.out.println("E/" + log);
+        System.out.println("[E][" + LocalTime.now() + "]" + log);
     }
 
     @Override
     public void warn(String log) {
-        System.out.println("W/" + log);
+        System.out.println("[W][" + LocalTime.now() + "]" + log);
     }
 
     @Override
     public void info(String log) {
-        System.out.println("I/" + log);        
+        System.out.println("[I][" + LocalTime.now() + "]" + log);
     }
 
     @Override
     public void debug(String log) {
-        System.out.println("D/" + log);        
+        System.out.println("[D][" + LocalTime.now() + "]" + log);
     }
 
     @Override
     public void trace(String log) {
-        System.out.println("T/" + log);        
+        System.out.println("[T][" + LocalTime.now() + "]" + log);
     }
 }
