a4aefe627f7ee9e69fa3ec57508e762ba1fcccf5
Anton Dukeman <anton.dukeman@gmail.com>
2022-03-07 23:36:48 -0800

    Added support for truncated times list

--- app/src/main/java/ppp/stats/messenger/message/UserMiniTimesMessage.java
+++ app/src/main/java/ppp/stats/messenger/message/UserMiniTimesMessage.java
@@ -10,11 +10,17 @@ import ppp.stats.models.IUser;
 public class UserMiniTimesMessage implements IBotMessage {
     public final IUser requestor;
     public final Map<LocalDate, Integer> timesMap;
+    public final int numRowsToSend;
 
     public UserMiniTimesMessage(IUser requestor, Map<LocalDate, Integer> timesMap) {
-            this.requestor = requestor;
-            this.timesMap = timesMap;
-        }
+        this(requestor, timesMap, timesMap.size());
+    }
+
+    public UserMiniTimesMessage(IUser requestor, Map<LocalDate, Integer> timesMap, int numRowsToSend) {
+        this.requestor = requestor;
+        this.timesMap = timesMap;
+        this.numRowsToSend = numRowsToSend;
+    }
 
     @Override
     public void send(IMessageClient msgClient, ITextChannel channel) {
--- app/src/main/java/ppp/stats/parser/command/TimesCommand.java
+++ app/src/main/java/ppp/stats/parser/command/TimesCommand.java
@@ -3,13 +3,36 @@ package ppp.stats.parser.command;
 import java.util.List;
 
 import ppp.stats.action.IAction;
+import ppp.stats.action.SendBasicMessageAction;
 import ppp.stats.action.SendMiniTimesAction;
 
 public class TimesCommand implements ICommand {
     @Override
     public IAction parse(List<String> command) {
-        if(command.size() == 0) { return new SendMiniTimesAction(); }
-        return null;
+        if (command.size() == 0) {
+            return new SendMiniTimesAction(10);
+        } else if (command.size() == 1) {
+            String option = command.get(0);
+            if (option.equals("all")) {
+                return new SendMiniTimesAction();
+            } else {
+                try {
+                    Double d = Double.parseDouble(option);
+                    if (d.intValue() > 0) {
+                        return new SendMiniTimesAction(d.intValue());
+                    } else {
+                        throw new NumberFormatException();
+                    }
+                } catch (NumberFormatException e) {
+                    return this.usageMessage();
+                }
+            }
+        }
+        return this.usageMessage();
+    }
+
+    private SendBasicMessageAction usageMessage() {
+        return new SendBasicMessageAction("```Usage: times [all | <number>]\n\t<number> must be positive```");
     }
 
     @Override
